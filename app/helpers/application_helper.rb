9ca82ad3d2ab10f724de8735b19ab2bb00200a61
Theo Ferguson <ferguson.theo@gmail.com>
2022-10-05 04:20:06 -0400

    expenditures and receipts columns, search filtering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
 
     def show_svg(path)
-        File.open("app/assets/images/#{path}", "rb") do |file|
-          raw file.read
-        end
+      File.open("app/assets/images/#{path}", "rb") do |file|
+        raw file.read
       end
+    end
+
+    def helpers
+      ActionController::Base.helpers
+    end
 
 end
