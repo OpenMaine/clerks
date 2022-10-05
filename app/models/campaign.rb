d795c9588d700a4926b3a9643e7acaa802e8e7d8
Theo Ferguson <ferguson.theo@gmail.com>
2022-10-05 10:13:24 -0400

    adding charter commission to office types

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,5 +2,5 @@ class Campaign < ApplicationRecord
   belongs_to :election
   has_many :campaign_reports
 
-  enum :office, [:Mayor, :"City Council", :"School Board"]
+  enum :office, [:Mayor, :"City Council", :"School Board", :"Charter Commission"]
 end
