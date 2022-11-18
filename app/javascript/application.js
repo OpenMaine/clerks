6c0e4aabd16000e5ebd285e6e2781b23ea43e7dd
Theo Ferguson <ferguson.theo@gmail.com>
2022-11-18 11:41:57 -0500

    small bootstrap change

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,10 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-//= require turbolinks
 //= require jquery
+//= rails-ujs
+//= require turbolinks
 //= require bootstrap-sprockets
-//= require jquery_ujs
-//= require bootstrap
 //= require_tree .
 
 console.log("hello")
